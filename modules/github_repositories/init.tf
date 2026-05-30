98dea4bd1b983f4266d1dac5e1bad47c0242a2b3
Rodman <rodman@stuhlmuller.net>
2026-05-30 13:13:16 -0700

    fix: require aws provider for ssm token

--- modules/github_repositories/init.tf
+++ modules/github_repositories/init.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 6.12.0"
