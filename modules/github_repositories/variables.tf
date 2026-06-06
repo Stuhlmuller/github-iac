7267b9b2ee94ec5b70b839de16c65977bcd0e2c6
Rodman <rodman@stuhlmuller.net>
2026-06-05 23:15:11 -0700

    fix: accept heterogeneous repository overrides

--- modules/github_repositories/variables.tf
+++ modules/github_repositories/variables.tf
@@ -10,38 +10,13 @@ variable "organization" {
 
 variable "repositories" {
   description = "Repository-specific overrides keyed by repository name. Existing repositories that are not listed here inherit default_repository_config."
-  type = map(object({
-    description                = optional(string)
-    visibility                 = optional(string)
-    homepage_url               = optional(string)
-    gitignore_template         = optional(string)
-    default_branch             = optional(string)
-    rename_default_branch      = optional(bool)
-    require_code_owner_reviews = optional(bool)
-    is_template                = optional(bool)
-    repository_template = optional(object({
-      owner                = string
-      repository           = string
-      include_all_branches = optional(bool)
-    }))
-    ruleset                     = optional(list(any))
-    delete_branch_on_merge      = optional(bool)
-    has_downloads               = optional(bool)
-    has_issues                  = optional(bool)
-    has_projects                = optional(bool)
-    has_wiki                    = optional(bool)
-    allow_update_branch         = optional(bool)
-    allow_auto_merge            = optional(bool)
-    allow_merge_commit          = optional(bool)
-    allow_squash_merge          = optional(bool)
-    squash_merge_commit_message = optional(string)
-    squash_merge_commit_title   = optional(string)
-    allow_rebase_merge          = optional(bool)
-    archived                    = optional(bool)
-    auto_init                   = optional(bool)
-    vulnerability_alerts        = optional(bool)
-  }))
-  default = {}
+  type        = any
+  default     = {}
+
+  validation {
+    condition     = can(keys(var.repositories))
+    error_message = "repositories must be an object or map keyed by repository name."
+  }
 }
 
 variable "default_repository_config" {
